43ac19748eed6e5646fc714f197fb1474f2e7cc8
3dpose <3d.pose.est@gmail.com>
2021-08-27 22:53:57 -0700

    added sorting and timing runtime

--- calculate_mupots_btmup.py
+++ calculate_mupots_btmup.py
@@ -1,25 +1,30 @@
-import torch 
-import numpy as np 
-from TorchSUL import Model as M 
+import torch
+import numpy as np
+from TorchSUL import Model as M
 
-import config 
+import config
 from lib import hmaputil
 from lib import btmuputil
 from lib.models import transnet
-from lib.models import networkbtmup 
+from lib.models import networkbtmup
 
-import glob 
-import cv2 
-import pickle 
-from tqdm import tqdm 
+import glob
+import cv2
+import pickle
+from tqdm import tqdm
+
+import time
 
 if __name__=='__main__':
+
+	start_time = time.time()
+
 	### Stage 1: Do bottom-up estimation
 	model = networkbtmup.HR3DNet(config.head_layernum, config.head_chn, config.upsample_layers, config.upsample_chn)
 	hmapGen = hmaputil.HMapGenerator('./mupots/est_p2ds/') # detection is applied on 1024 size
 
 	with torch.no_grad():
-		# initialize model 
+		# initialize model
 		x = torch.zeros(1,3+17,config.inp_size,config.inp_size)
 		model(x)
 		M.Saver(model).restore('./ckpts/model_btmup/')
@@ -29,19 +34,19 @@ if __name__=='__main__':
 		res_all = {}
 		for i in range(20):
 			print('Seq:',i)
-			imgs = glob.glob('./MultiPersonTestSet/TS%d/*.jpg'%(i+1))
+			imgs = sorted(glob.glob('./MultiPersonTestSet/TS%d/*.jpg'%(i+1)))
 			buff = []
 			for frame_idx,imgname in enumerate(tqdm(sorted(imgs))):
-				imgname = imgname.replace('\\','/')  # for windows users 
+				imgname = imgname.replace('\\','/')  # for windows users
 				img = cv2.imread(imgname)
 				pts, scores, roots, rels = btmuputil.run_pipeline(img, model, hmap_generator=hmapGen, vid_idx=i, frame_idx=frame_idx)
 				pts_final = btmuputil.get_pts3d(pts, roots, rels)
 				buff.append([imgname, pts_final, scores])
-			res_all[i+1] = buff 
+			res_all[i+1] = buff
 		pickle.dump(res_all, open('mupots/MUPOTS_Preds_btmup.pkl','wb'))
 
 	### Stage 2: Transform 14 pts (MUCO format) to 17 pts (mpii format)
-	# initialize models 
+	# initialize models
 	linear_fit = transnet.LinearModel()
 	trans_net = transnet.TransNet()
 	x_dumb = torch.from_numpy(np.zeros([2, config.num_pts, 3], dtype=np.float32))
@@ -49,7 +54,7 @@ if __name__=='__main__':
 	x_dumb = torch.from_numpy(np.zeros([2, config.num_pts*3], dtype=np.float32))
 	trans_net(x_dumb)
 
-	# load models 
+	# load models
 	saver_liner = M.Saver(linear_fit)
 	saver_trans = M.Saver(trans_net)
 	saver_liner.restore('./ckpts/model_transform/model_linear/')
@@ -76,3 +81,7 @@ if __name__=='__main__':
 
 	pickle.dump(data, open('./mupots/MUPOTS_Preds_btmup_transformed.pkl', 'wb'))
 
+	end_time = time.time()
+	hours, rem = divmod(end_time-start_time, 3600)
+	minutes, seconds = divmod(rem, 60)
+	print("{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds))
